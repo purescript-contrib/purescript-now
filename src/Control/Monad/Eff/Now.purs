def4da2229b1534adc3527f1e6b11b000691b338
Gary Burgess <gary.burgess@gmail.com>
2016-06-02 02:03:46 +0100

    Initial commit

---
+++ src/Control/Monad/Eff/Now.purs
@@ -0,0 +1,11 @@
+module Control.Moand.Eff.Now where
+
+import Control.Monad.Eff (Eff)
+import Data.DateTime.Instant (Instant)
+
+-- | Effect type for when accessing the current date/time.
+foreign import data NOW :: !
+
+-- | Gets an `Instant` value corresponding to the current date/time according to
+-- | the current machine’s clock.
+foreign import now :: forall e. Eff (now :: NOW | e) Instant
