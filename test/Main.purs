c705e77cde5121ff15b8f1cd00a0c909c09da5ca
Thomas Honeyman <admin@thomashoneyman.com>
2021-11-17 09:24:18 -0500

    Run purs-tidy

--- test/Main.purs
+++ test/Main.purs
@@ -17,28 +17,27 @@ type FailedTestCount = Int
 type Test = StateT FailedTestCount Effect Unit
 
 test :: String -> Effect Unit -> Test
-test name body = 
+test name body =
   try (lift body) >>= case _ of
     Right _ -> log ("✔️  " <> name)
-    Left err -> do 
+    Left err -> do
       log ("❌  " <> name)
       logShow err
       void $ modify (_ + 1)
 
-assertDoesNotThrow :: ∀ a. Effect a -> Effect Unit
+assertDoesNotThrow :: forall a. Effect a -> Effect Unit
 assertDoesNotThrow action = try action >>= case _ of
   Right _ -> pure unit
   Left err -> do throw ("Expected to not throw an exception, but one was thrown.\n" <> show err)
 
-
-timeAdvances :: ∀ time. Show time => Ord time => Effect time -> String -> Test
+timeAdvances :: forall time. Show time => Ord time => Effect time -> String -> Test
 timeAdvances getTime name = test ("time advances with " <> name) do
   startTime <- getTime
   endTime <- getTime
-  assert' (show endTime <> " should be greater than than " <> show startTime) $ endTime >= startTime 
+  assert' (show endTime <> " should be greater than than " <> show startTime) $ endTime >= startTime
 
 canGetTheCurrentTime :: Test
-canGetTheCurrentTime = test "can get the current time" do 
+canGetTheCurrentTime = test "can get the current time" do
   assertDoesNotThrow nowTime
 
 offsetSeemsSensible :: Test
@@ -47,14 +46,13 @@ offsetSeemsSensible = test "UTC offset between -13:00 and +15:00" do
   assert $ between lbound ubound minutes
 
   where
-    lbound = Minutes $ (-13.0) * minPerHour
-    ubound = Minutes $ 15.0 * minPerHour
-    minPerHour = 60.0
-    
+  lbound = Minutes $ (-13.0) * minPerHour
+  ubound = Minutes $ 15.0 * minPerHour
+  minPerHour = 60.0
 
 main :: Effect Unit
 main = do
-  failedTests <- flip execStateT 0 do 
+  failedTests <- flip execStateT 0 do
     timeAdvances now "now"
     timeAdvances nowDate "nowDate"
     canGetTheCurrentTime
